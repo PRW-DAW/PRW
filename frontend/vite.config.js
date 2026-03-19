9f6e71ff9b21c5b5b471ce10ce431000be3abd04
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-03-19 15:33:30 +0000

    Update vite.config.js (allowedHosts)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'devhub.com',
+      'devhub.pro',
+      'api.devhub.com',
+      'api.devhub.pro',
+      'www.devhub.com',
+      'www.devhub.pro',
+      'www.api.devhub.com',
+      'www.api.devhub.pro',
+    ],
+  },
 })
